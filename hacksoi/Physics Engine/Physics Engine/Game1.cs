129eaa3d4d2b394d37087b685618e26f4948e18e
hacksoi <sohackinp@yahoo.com>
2016-02-15 19:37:45 -0800

    tiled test

---
+++ hacksoi/Physics Engine/Physics Engine/Game1.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using TiledSharp;
+using System;
+
+namespace Physics_Engine
+{
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        TmxMap map;
+        Texture2D tileset;
+
+        int tileWidth;
+        int tileHeight;
+        int tilesetTilesWide;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+
+            map = new TmxMap("test.tmx");
+            graphics.PreferredBackBufferWidth = map.Width * map.TileWidth;
+            graphics.PreferredBackBufferHeight = map.Height * map.TileHeight;
+
+            this.IsMouseVisible = true;
+            Content.RootDirectory = "Content";
+        }
+        
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+        
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            tileset = Content.Load<Texture2D>(map.Tilesets[0].Name.ToString());
+            
+            tileWidth = map.Tilesets[0].TileWidth;
+            tileHeight = map.Tilesets[0].TileHeight;
+
+            tilesetTilesWide = tileset.Width / tileWidth - 1;
+        }
+        
+        protected override void UnloadContent()
+        {
+        }
+        
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            base.Update(gameTime);
+        }
+        
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+
+            for(int i = 0; i < map.Layers[1].Tiles.Count; i++)
+            {
+                int gid = map.Layers[1].Tiles[i].Gid;
+
+                if(gid == 0)
+                {
+
+                }
+                else
+                {
+                    int tileFrame = gid - 1;
+                    int column = tileFrame % tilesetTilesWide;
+                    int row = tileFrame / tilesetTilesWide;
+
+                    float x = (i % map.Width) * map.TileWidth;
+                    float y = (i / map.Width) * map.TileWidth;
+
+                    Rectangle tilesetRec = new Rectangle(tileWidth * column + column, tileHeight * row + row, tileWidth, tileHeight);
+
+                    spriteBatch.Draw(tileset, new Rectangle((int)x, (int)y, tileWidth, tileHeight), tilesetRec, Color.White);
+                }
+            }
+
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
